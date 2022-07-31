357108a11c39cb13c8ddf0d0e68dffb079629367
Gauravbots <102266943+Gauravbots@users.noreply.github.com>
2022-07-31 05:31:14 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -17,6 +17,6 @@ async def refunc(client, message):
        elif str(media) == "MessageMediaType.AUDIO":
            button.append([InlineKeyboardButton("🎵 𝙰𝙾𝚄𝙳𝙸𝙾",callback_data = "upload_audio")])
        await message.reply_text(
-          f""🥳 Your file is successfully renamed**\n**Output FileName :- {new_name}",
+          f"🥳 Your file is successfully renamed**\n**Output FileName :- {new_name}",
           reply_to_message_id=file.id,
           reply_markup=InlineKeyboardMarkup(button))
